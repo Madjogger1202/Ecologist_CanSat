712b2b6cd97a7dece026ddbad61becac8500cd52
Madjogger1202 <61242548+Madjogger1202@users.noreply.github.com>
2020-02-25 16:25:05 +0300

    Create ME2-O2.ino

---
+++ tests/O2_test/ME2-O2/ME2-O2.ino
@@ -0,0 +1,45 @@
+
+const float VRefer = 3.3;       // voltage of adc reference
+const int pinAdc   = A0;
+float Vout =0;
+void setup() 
+{
+    Serial.begin(9600);
+}
+
+void loop() 
+{
+    Serial.print("Vout =");
+
+    Vout = readO2Vout();
+    Serial.print(Vout);
+    Serial.print(" V, Concentration of O2 is ");
+    Serial.println(readConcentration());
+    delay(500);
+}
+
+float readO2Vout()
+{
+    long sum = 0;
+    for(int i=0; i<32; i++)
+    {
+        sum += analogRead(pinAdc);
+    }
+
+    sum >>= 5;
+
+    float MeasuredVout = sum * (VRefer / 1023.0);
+    return MeasuredVout;
+}
+
+float readConcentration()
+{
+    // Vout samples are with reference to 3.3V
+    float MeasuredVout = readO2Vout();
+
+    //float Concentration = FmultiMap(MeasuredVout, VoutArray,O2ConArray, 6);
+    //when its output voltage is 2.0V,
+    float Concentration = MeasuredVout * 0.21 / 2.0;
+    float Concentration_Percentage=Concentration*100;
+    return Concentration_Percentage;
+}
