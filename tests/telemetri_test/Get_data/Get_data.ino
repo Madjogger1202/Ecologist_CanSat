2807b6032e66455296b712d878163ba0cb0416ed
Madjogger1202 <61242548+Madjogger1202@users.noreply.github.com>
2020-04-18 17:07:06 +0300

    .

--- tests/telemetri_test/Get_data/Get_data.ino
+++ tests/telemetri_test/Get_data/Get_data.ino
@@ -15,10 +15,10 @@ struct telemetry        //Создаем структуру
 }data; 
 void setup()
 {
-    Serial.begin(9600);                                    
+    Serial.begin(115200);                                    
     radio.begin();                                        // Инициируем работу nRF24L01+
-    radio.setChannel(126);                                // Указываем канал приёма данных (от 0 до 127), 5 - значит приём данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
-    radio.setDataRate     (RF24_2MBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
+    radio.setChannel(120);                                // Указываем канал приёма данных (от 0 до 127), 5 - значит приём данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
+    radio.setDataRate     (RF24_250KBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
     radio.setPALevel      (RF24_PA_HIGH);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
     radio.openReadingPipe (1, 0x1234567899LL);            // Открываем 1 трубу с идентификатором 0x1234567890 для приема данных (на ожном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
     radio.startListening  ();                             // Включаем приемник, начинаем прослушивать открытую трубу
@@ -28,17 +28,28 @@ void loop()
     if(radio.available())
     {                                
        radio.read(&data, sizeof(data));                   // читаем данные и указываем сколько байт читать
-       Serial.print("Temperature (ds18b20):");            // 
-       Serial.println(data.temp_str);                     // пишем в порт температуру 
-       Serial.print("Pressure (bmp280):");                // 
-       Serial.println(data.press_str*4);                  // пишем в порт давление с барометра
-       Serial.print("Temperature (bmp280):");             //
-       Serial.println(data.bmp_temp_str);                 // пишем в порт температуру с барометра
-       Serial.print("X (adxl345):");                      //
-       Serial.println(data.x_str);                        // пишем в порт ускорение по оси X
-       Serial.print("Y (adxl345):");                      // 
-       Serial.println(data.y_str);                        // пишем в порт ускорение по оси Y
-       Serial.print("Z (adxl345):");                      //
-       Serial.println(data.z_str);                        // пишем в порт ускорение по оси Z
+    //   Serial.print("Temperature (ds18b20):");            // 
+    //   Serial.println(data.temp_str);                     // пишем в порт температуру 
+   //    Serial.print("Pressure (bmp280):");                // 
+  //     Serial.println(data.press_str*4);                  // пишем в порт давление с барометра
+ //      Serial.print("Temperature (bmp280):");             //
+       Serial.print("$");
+       Serial.println(data.press_str*4); 
+   //     Serial.println(data.press_str*4); 
+   //    Serial.print(data.bmp_temp_str);                 // пишем в порт температуру с барометра
+  //     Serial.print("X (adxl345):");                      //
+     //  Serial.print(" ");                      //
+   //   Serial.println(data.x_str*3);  
+  //    Serial.print(" ");  // пишем в порт ускорение по оси X
+   //    Serial.print("Y (adxl345):");                      //   
+  //    Serial.println(data.y_str*3);  
+  //    Serial.print(" ");  // пишем в порт ускорение по оси Y
+   //    Serial.print("Z (adxl345):");                      //
+  //     Serial.println(data.z_str*3);                        // пишем в порт ускорение по оси Z
+    //   Serial.println("");                        // пишем в порт ускорение по оси Z
+   //    Serial.println("");    
+   //    Serial.println("");    
+     //  Serial.println("");    
+      Serial.print(";");    
     }
 }
