a594fd1a859f5ced48f6d8e78ed6f6187500829e
Madjogger1202 <61242548+Madjogger1202@users.noreply.github.com>
2020-05-02 16:55:32 +0300

    Update Send_data.ino

--- tests/telemetri_test/Send_data/Send_data.ino
+++ tests/telemetri_test/Send_data/Send_data.ino
@@ -1,27 +1,26 @@
 #define Radio_CE 40  //40                 // пин chip enable для радио
 #define Radio_CSN 41     //41             // пин chip select для радио
-//#define Acsel_pin 43                  // пин chip select для акселерометра
-//#define Temperature_pin 42            // пин chip select для термометра
-//#define Pressure_pin 44               // пин chip select для баометра
-//#define Sd_pin 39                     // пин chip select для SD карты
+#define Acsel_pin 43                  // пин chip select для акселерометра
+#define Temperature_pin 42            // пин chip select для термометра
+#define Pressure_pin 44               // пин chip select для баометра
+#define Sd_pin 39                     // пин chip select для SD карты
 
 #define telemetri_rate 300 // задержка между отправкой пакетов, задаётся в милисекундах 
 
-//#include "microDS18B20.h"
-//#include "Adafruit_BMP280.h"
-//#include "SparkFun_ADXL345.h"   
+#include "microDS18B20.h"
+#include "Adafruit_BMP280.h"
+#include "SparkFun_ADXL345.h"   
 #include <nRF24L01.h>                                    
 #include <RF24.h>  
-//#include <SD.h>
+#include <SD.h>
 RF24 radio(Radio_CE, Radio_CSN);                              // Создаём объект radio для работы с библиотекой RF24, указывая номера выводов nRF24L01+ (CE, CSN)
-//Adafruit_BMP280 bmp(Pressure_pin);                            // создаём объект bmp для работы с барометром
-//MicroDS18B20 t_sensor(Temperature_pin);                       // создаём объект t_sensor для работы с термометром
-//ADXL345 adxl = ADXL345(Acsel_pin);                            // создаём объект adxl для работы с акселерометром
+Adafruit_BMP280 bmp(Pressure_pin);                            // создаём объект bmp для работы с барометром
+MicroDS18B20 t_sensor(Temperature_pin);                       // создаём объект t_sensor для работы с термометром
+ADXL345 adxl = ADXL345(Acsel_pin);                            // создаём объект adxl для работы с акселерометром
 
-//long int timer;                                               // переменная для подсчета выполненных циклов программы
-//int last_temperature;                                         // переменная для сверки значений с термометра
-//bool lst;                                                     // флаг для обработки ошибок
-//int x,y,z;                                                    // переменные для ускорений по 3 осям
+int last_temperature;                                         // переменная для сверки значений с термометра
+bool lst;                                                     // флаг для обработки ошибок
+int x,y,z;                                                    // переменные для ускорений по 3 осям
 
 struct telemetry        //Создаем структуру
 {                  
@@ -31,12 +30,13 @@ struct telemetry        //Создаем структуру
    int x_str;           ////////////////////////////////////////////////
    int y_str;           //  переменные для ускорений с акселерометра  //
    int z_str;           ////////////////////////////////////////////////
+   long int timer;      // переменная для подсчета выполненных циклов программы
 }data; 
 void setup(){
- //  pinMode(Sd_pin, OUTPUT);                                   // настройка chip select катрочки на отправку
+   pinMode(Sd_pin, OUTPUT);                                   // настройка chip select катрочки на отправку
    SPI.begin();                                               // инициализируем работу с SPI 
    SPI.setDataMode(SPI_MODE3);                                // насотройка SPI
-//   SD.begin(Sd_pin);                                          // инициализация sd карты 
+   SD.begin(Sd_pin);                                          // инициализация sd карты 
    delay(100);                                                // задержка для уверенности в успешности инициализации 
    radio.begin();                                             // Инициируем работу nRF24L01+
    radio.setChannel(120);                                     // Указываем канал передачи данных (от 0 до 127), 5 - значит передача данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
@@ -45,62 +45,62 @@ void setup(){
    radio.openWritingPipe(0x1234567899LL);                     // Открываем трубу с идентификатором 0x1234567890 для передачи данных (на одном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)           
    
    
-//   bmp.begin();
- //  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* режим работы */
- //                  Adafruit_BMP280::SAMPLING_X2,     /* коэф. температуры */
- //                  Adafruit_BMP280::SAMPLING_X16,    /* коэф. давления */
- //                  Adafruit_BMP280::FILTER_X16,      /* фильтр */
- //                  Adafruit_BMP280::STANDBY_MS_500); /* время ожидания */
+   bmp.begin();
+   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* режим работы */
+                   Adafruit_BMP280::SAMPLING_X2,     /* коэф. температуры */
+                   Adafruit_BMP280::SAMPLING_X16,    /* коэф. давления */
+                   Adafruit_BMP280::FILTER_X16,      /* фильтр */
+                   Adafruit_BMP280::STANDBY_MS_500); /* время ожидания */
 
- //  adxl.powerOn();                     // вывод датчика из режима пониженного энергопотребления
- //  adxl.setRangeSetting(16);           // настройка чувствительности (макс - 16)
-  // t_sensor.requestTemp();             // отправляем запрос на температуру
+   adxl.powerOn();                     // вывод датчика из режима пониженного энергопотребления
+   adxl.setRangeSetting(16);           // настройка чувствительности (макс - 16)
+   t_sensor.requestTemp();             // отправляем запрос на температуру
    delay(1000);                        // задержка для адекватных значений температуры 
 }
 void loop()
 {
 
-//   adxl.readAccel(&x, &y, &z);                  // запись значений для ускорений в указанные переменные
- //  data.temp_str = t_sensor.getTemp();          // запись в структурную переменную телеметрии значений температуры
- //  data.bmp_temp_str = bmp.readTemperature();   // запись в структурную переменную телеметрии значений температуры с барометра
- //  data.press_str = bmp.readPressure()/4;       // запись в структурную переменную телеметрии значений температуры с барометра
- //  data.x_str = x;                              /////////////////////////////////////////////////////////////
- //  data.y_str = y;                              //   запись в структурную переменную телеметрии ускорений  //
- //  data.z_str = z;                              /////////////////////////////////////////////////////////////
+   adxl.readAccel(&x, &y, &z);                  // запись значений для ускорений в указанные переменные
+   data.temp_str = t_sensor.getTemp();          // запись в структурную переменную телеметрии значений температуры
+   data.bmp_temp_str = bmp.readTemperature();   // запись в структурную переменную телеметрии значений температуры с барометра
+   data.press_str = bmp.readPressure()/4;       // запись в структурную переменную телеметрии значений температуры с барометра
+   data.x_str = x;                              /////////////////////////////////////////////////////////////
+   data.y_str = y;                              //   запись в структурную переменную телеметрии ускорений  //
+   data.z_str = z;                              /////////////////////////////////////////////////////////////
    
  //////////////////////////// обработка багов термометра ///////////////////////////////////////////////////////////////////
-//  if(timer == 4) last_temperature =  data.temp_str;                                                                       //
-//  if (timer > 4) {                                                                                                        //  
-//    if  ( (( ( data.temp_str>400) || ( data.temp_str< 0) )||(abs( data.temp_str-last_temperature)>=19))&&(!lst)) {        //
-//      data.temp_str=last_temperature;                                                                                     //
-//      lst=1;                                                                                                              //
-//    }                                                                                                                     //
-//    else {                                                                                                                //
- //   lst=0;                                                                                                                //
- //   last_temperature= data.temp_str;                                                                                      //
- //  }                                                                                                                      //
-//  }                                                                                                                      //
+  if(data.timer == 4) last_temperature =  data.temp_str;                                                                       //
+  if (data.timer > 4) {                                                                                                        //  
+    if  ( (( ( data.temp_str>400) || ( data.temp_str< 0) )||(abs( data.temp_str-last_temperature)>=19))&&(!lst)) {        //
+      data.temp_str=last_temperature;                                                                                     //
+      lst=1;                                                                                                              //
+    }                                                                                                                     //
+    else {                                                                                                                //
+    lst=0;                                                                                                                //
+    last_temperature= data.temp_str;                                                                                      //
+   }                                                                                                                      //
+  }                                                                                                                      //
  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
  
    radio.write(&data, sizeof(data));       //  отправка в эфир пакета данных
-//   t_sensor.requestTemp();                 //  запрос температуры
+   t_sensor.requestTemp();                 //  запрос температуры
 
-//  File dataFile = SD.open("datalog.csv", FILE_WRITE);  // открываем для записи файл, если его нет - создаём
-//  if (dataFile)                                        // проверка, что пишем не в воздух
-//  {
-//    dataFile.print(data.press_str);            ///////////////////////////  
-//    dataFile.print(", ");                      //
-//    dataFile.print(data.temp_str);             //
-//    dataFile.print(", ");                      //   
-//    dataFile.print(data.x_str);                //
-//    dataFile.print(", ");                      //   запись  телеметрии на сд карту 
-//    dataFile.print(data.y_str);                //
-//    dataFile.print(", ");                      //
-//    dataFile.print(data.z_str);                //
-//    dataFile.print(", ");                      //
-//    dataFile.println(data.bmp_temp_str);       ///////////////////////////    
-//    dataFile.close();                          // для подтверждения записи 
-//  }
+  File dataFile = SD.open("datalog.csv", FILE_WRITE);  // открываем для записи файл, если его нет - создаём
+  if (dataFile)                                        // проверка, что пишем не в воздух
+  {
+    dataFile.print(data.press_str);            ///////////////////////////  
+    dataFile.print(", ");                      //
+    dataFile.print(data.temp_str);             //
+    dataFile.print(", ");                      //   
+    dataFile.print(data.x_str);                //
+    dataFile.print(", ");                      //   запись  телеметрии на сд карту 
+    dataFile.print(data.y_str);                //
+    dataFile.print(", ");                      //
+    dataFile.print(data.z_str);                //
+    dataFile.print(", ");                      //
+    dataFile.println(data.bmp_temp_str);       ///////////////////////////    
+    dataFile.close();                          // для подтверждения записи 
+  }
    delay(telemetri_rate);                      // задержка для отправки данных 
-//   timer++;                                    // + 1 выполненный цикл
+   data.timer++;                                    // + 1 выполненный цикл
 }
