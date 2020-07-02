7feb890f2b05c0ae2d4c84864effc65d756f8cb6
Madjogger1202 <61242548+Madjogger1202@users.noreply.github.com>
2020-07-02 22:14:22 +0300

    Update Dose_counterV1.ino

--- tests/dozimetr_test/Dose_counterV1/Dose_counterV1.ino
+++ tests/dozimetr_test/Dose_counterV1/Dose_counterV1.ino
@@ -1,14 +1,25 @@
 volatile int counter = 0;  // переменная-счётчик
-void setup() {
-  Serial.begin(9600); 
-  pinMode(2, INPUT); 
-  attachInterrupt(0, buttonTick, FALLING);
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(2, INPUT); // 2 пин поддерживает прерывания = на многих платах ардуино
+  attachInterrupt(0, buttonTick, FALLING); // 0 - это номер прерывания, а не номер пина, про прерывания почитать можно туть: https://alexgyver.ru/lessons/interrupts/
 }
-void buttonTick() {
+
+void buttonTick()
+{
   counter++;  // + нажатие
 }
-void loop() {
-  for(int i =0; i<counter; i++)
+
+void loop()
+{ 
+  int limit = map(counter, 0, 200, 0, 20); // синтаксис функции: источник, 
+                                           //мин знач. из источника 
+                                           //макс знач из источника
+                                           //мин значение для новой переменной
+                                           //макс знач для новой переменной
+                                           // фукция служит для того, чтобы не высчитывать каждый раз новый коэф., а так - это просто деление с опциональной инверсией
+  for (int i = 0; i < limit; i++)
   {
     Serial.print("#");
   }
